e4b13b2db0030e9eb7615a3ab806f2b11dda6cd9
Ahmed Nasr <ahmed.harg@yahoo.com>
2022-10-01 14:39:01 +0200

    backup of all without geofence

---
+++ Project4_backup_with_no_geofence/starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -0,0 +1,80 @@
+package com.udacity.project4.locationreminders.savereminder
+
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import com.udacity.project4.R
+import com.udacity.project4.base.BaseFragment
+import com.udacity.project4.base.NavigationCommand
+import com.udacity.project4.databinding.FragmentSaveReminderBinding
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+
+class SaveReminderFragment : BaseFragment() {
+    //Get the view model this time as a single to be shared with the another fragment
+    override val _viewModel: SaveReminderViewModel by inject()
+    private lateinit var binding: FragmentSaveReminderBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_save_reminder, container, false)
+
+        setDisplayHomeAsUpEnabled(true)
+
+        binding.viewModel = _viewModel
+
+        Toast.makeText(requireContext(),"Please enter reminder data!",Toast.LENGTH_SHORT).show()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        binding.selectLocation.setOnClickListener {
+            //            Navigate to another fragment to get the user location
+            _viewModel.navigationCommand.value =
+                NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
+        }
+
+        binding.saveReminder.setOnClickListener {
+            val title = _viewModel.reminderTitle.value
+            val description = _viewModel.reminderDescription.value
+            val location = _viewModel.reminderSelectedLocationStr.value
+            val latitude = _viewModel.latitude.value
+            val longitude = _viewModel.longitude.value
+            val reminder = ReminderDTO(title,description,location,latitude,longitude)
+
+//            TODO: use the user entered reminder details to:
+//             1) add a geofencing request
+//             2) save the reminder to the local db
+
+            //2.DONE
+            CoroutineScope(Dispatchers.IO) .launch{
+                _viewModel.dataSource.saveReminder(reminder)
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //make sure to clear the view model after destroy, as it's a single view model.
+        _viewModel.onClear()
+    }
+}
